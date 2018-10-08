e274d59d90e76c0c696e76fc3aa2a3f5dbe8a358
MichaelArbel <michael.n.arbel@gmail.com>
2018-10-08 12:57:14 +0100

    SNDIscriminator: last layer shouldn’t use sn by default

--- gan/core/architecture.py
+++ gan/core/architecture.py
@@ -403,7 +403,7 @@ class SNGANDiscriminator(Discriminator):
         c2_1 = lrelu(conv2d(c2_0, 512, 4, 4, 2, 2, with_sn=self.with_sn, with_learnable_sn_scale=self.with_learnable_sn_scale, update_collection=update_collection, stddev=0.02, name=self.prefix + 'c2_1', data_format=self.format,with_singular_values=True))
         c3_0 = lrelu(conv2d(c2_1, 512, 3, 3, 1, 1, with_sn=self.with_sn, with_learnable_sn_scale=self.with_learnable_sn_scale, update_collection=update_collection, stddev=0.02, name=self.prefix + 'c3_0', data_format=self.format,with_singular_values=True))
         c3_0 = tf.reshape(c3_0, [batch_size, -1])
-        l4 = linear(c3_0, self.o_dim, with_sn=True, update_collection=update_collection, stddev=0.02, name=self.prefix + 'l4')
+        l4 = linear(c3_0, self.o_dim, with_sn=self.with_sn, with_learnable_sn_scale=self.with_learnable_sn_scale, update_collection=update_collection, stddev=0.02, name=self.prefix + 'l4')
         return {'h0': c0_0, 'h1': c0_1, 'h2': c1_0, 'h3': c1_1, 'h4': c2_0, 'h5': c2_1, 'h6': c3_0, 'hF': l4}
 
 
